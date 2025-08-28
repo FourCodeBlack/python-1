4cec1ad9d812b34bb846ba83affc9770fbfa9350
baguskrisna-dcotor <bagusganteng24434@gmail.com>
2025-08-28 15:26:10 +0700

    rectangle

--- main2.py
+++ main2.py
@@ -1,14 +1,17 @@
 i= 5
 
-# while i > 0:
-#     print("*"*i)
-#     i-=1
+while i > 0:
+    print("*"*i)
+    i-=1
+
 i=1
 j = 5
 while i<=9:
     print(" "*j,"*"*i)
     j-=1
     i+=2
+    
+i = 0
     
     
     

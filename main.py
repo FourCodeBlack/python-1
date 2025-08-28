b57b59f3441c57d18831c8d57a8147206b549580
baguskrisna-dcotor <bagusganteng24434@gmail.com>
2025-08-28 14:51:20 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a = int(input("masukkan nilai kamu"))
+
+if a > 100 :
+    print("NILAI TIDAK VALID")
+elif a >= 90 and a <=100 :
+    print("MANTAP")
+else:
+    print("gagal")
---
+++ main2.py
@@ -0,0 +1,15 @@
+i= 5
+
+# while i > 0:
+#     print("*"*i)
+#     i-=1
+i=1
+j = 5
+while i<=9:
+    print(" "*j,"*"*i)
+    j-=1
+    i+=2
+    
+    
+    
+    
